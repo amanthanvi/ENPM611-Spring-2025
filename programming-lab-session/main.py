5750bd21fdec1babb60b28a60f297b18e9d34c8d
amanthanvi <aman_thanvi@outlook.com>
2025-02-28 11:34:21 -0500

    Finished data_load.py and updated main with import

--- programming-lab-session/main.py
+++ programming-lab-session/main.py
@@ -1,3 +1,5 @@
+from ml_code.data_load import load_data
+
 # First, open the config.json file and load it into a variable called config using json.load()
 import json
 with open('config.json') as config_file:
@@ -5,18 +7,19 @@ with open('config.json') as config_file:
 
 # Step 1: Load Data
 # Use the relative path 'data/data.json' to load the data into a variable called data
-data = load_data("data/data.json")
+
+data = load_data("data/data.csv")
 print(data.head())  # Print the first 5 rows of the data
 
-# Step 2: Preprocess Data
-X_train, X_test, y_train, y_test = preprocess_data(data)
+# # Step 2: Preprocess Data
+# X_train, X_test, y_train, y_test = preprocess_data(data)
 
-# Step 3: Get the Model from ModelFactory
-model = ModelFactory.get_model(config["model_type"])
+# # Step 3: Get the Model from ModelFactory
+# model = ModelFactory.get_model(config["model_type"])
 
-# Step 4: Train and Evaluate the Model
-accuracy, cm, y_test, y_prob = train_and_evaluate(
-    model, X_train, X_test, y_train, y_test)
+# # Step 4: Train and Evaluate the Model
+# accuracy, cm, y_test, y_prob = train_and_evaluate(
+#     model, X_train, X_test, y_train, y_test)
 
-# Step 5: Print Metrics
-print_metrics(accuracy, cm, y_test, y_prob)  # cm is confusion matrix
+# # Step 5: Print Metrics
+# print_metrics(accuracy, cm, y_test, y_prob)  # cm is confusion matrix
--- programming-lab-session/ml_code/data_load.py
+++ programming-lab-session/ml_code/data_load.py
@@ -1,3 +1,17 @@
 # || START LECTURE NOTES ||
 # This is where we will be loading our csv file from data/data.csv
 # || END LECTURE NOTES ||
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def load_data(file_path: str):
+    try:
+        df = pd.read_csv(file_path)
+        sns.countplot(x='Attrition', data=df)
+        plt.show()
+        return df
+    except Exception as e:
+        print(f"Error loading data: {e}")
+        return None
